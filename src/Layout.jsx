92c765370057102f17d51021c094f95936b5e2fb
Ajay Negi <ajaynegi3345@icloud.com>
2024-05-10 22:36:38 +0530

    Update

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,20 +1,21 @@
-import React, { lazy , Suspense} from 'react'
+import React, {Suspense} from 'react'
 import { Outlet } from 'react-router-dom'
+import Github from "./components/Github.jsx";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 import LoadInfo from "./components/LoadInfo.jsx";
-// import BuyMeACoffeeWidget from "./components/BuyMeACoffeeWidget.jsx";
-const Navbar = lazy(() => import("./components/Navbar.jsx"));
-const Footer = lazy(()=> import("./components/Footer.jsx"));
+
 function Layout() {
     return (
         <>
             <Suspense fallback={<div className={"loading"}> <img src={"/loading.svg"}/> </div>}>
                 <Navbar/>
+                <Github />
                 <LoadInfo/>
                 <React.StrictMode>
                     <Outlet />
                 </React.StrictMode>
                 <Footer/>
-                {/*<BuyMeACoffeeWidget />*/}
             </Suspense>
         </>
     )
---
+++ src/components/Github.jsx
@@ -0,0 +1,11 @@
+import "./github.css";
+
+function Github() {
+    return (
+        <div className="github-container">
+
+        </div>
+    );
+}
+
+export default Github;
